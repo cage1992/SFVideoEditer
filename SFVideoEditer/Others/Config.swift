dd485988afa77f53b2dfca98553646ad80a7fb55
cage1992 <shyanshelley@imac-shelley.local>
2017-02-06 13:08:29 +0800

    333
    4444

--- SFVideoEditer/Others/Config.swift
+++ SFVideoEditer/Others/Config.swift
@@ -83,7 +83,7 @@ public func ShowLoading() {
     if loadingCover == nil  {
         loadingCover = UIView.init(frame: (UIApplication.shared.keyWindow?.bounds)!)
         loadingCover?.backgroundColor = .black
-        loadingCover?.alpha = 0.7
+        loadingCover?.alpha = 0.5
         UIApplication.shared.keyWindow?.addSubview(loadingCover!)
     }
     
